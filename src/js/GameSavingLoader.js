158dd413b38505a45e029d143b6c1c984c958b09
Roman Prilepsky <r616on@gmail.com>
2021-05-20 23:10:44 +0300

    done_1_2

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,9 +1,11 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
-    const buffer = await read();
-    return json(buffer);
+    let jsonProm = await read();
+    jsonProm = await json(jsonProm);
+    return new GameSaving(jsonProm);
   }
 }
--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,7 +1,9 @@
 import GameSavingLoader from '../GameSavingLoader';
+import GameSaving from '../GameSaving';
 
 test('test GameSavingLoader ', () => GameSavingLoader.load().then((saving) => {
-  expect(saving).toBe(
-    '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}',
+  const data = new GameSaving(
+    '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}',
   );
+  expect(saving).toEqual(expect.objectContaining(data));
 }));
